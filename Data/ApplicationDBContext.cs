705d40b1a3f2affbcfd77de94eb601b83d332146
Pavan_Fernando <pavanfernando6@gmail.com>
2024-05-09 22:10:50 +0530

    started coding

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using student_management_backend.Models.Entities;
+
+namespace student_management_backend.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace student_management_backend.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; } // if we added this required key then it shouldn't be null
+        public string? Address { get; set; } // can be nullable this String
+        public int Age { get; set; }
+
+    }
+}
